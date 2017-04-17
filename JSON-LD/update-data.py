b9bf7e0303fcdbc8c98a168a52d4610aab65f877
Christopher Diaz <crdiaz3@asu.edu>
2017-04-16 17:02:13 -0700

    applied ids to data for improved SPARQL queries

--- JSON-LD/update-data.py
+++ JSON-LD/update-data.py
@@ -1,12 +1,43 @@
 import json
 
-with open('city-data.jsonld') as data_file:
-    data = json.load(data_file)
+# open json file
+def openFile(filename):
+    with open(filename) as data_file:
+        return json.load(data_file)
 
-object_id = '@id'
-data[object_id] = 'SustainLD'
+# method to store updated data
+def storeData(filename, updated_data):
+    with open(filename, 'w') as outfile:
+        json.dump(updated_data, outfile, indent=4)
 
-with open('city-data.jsonld', 'w') as outfile:
-    json.dump(data, outfile, indent=4)
+# method to apply ids
+def update(cur_data):
+    # variables
+    ID = '@id'
 
-print(data['@id'])
+    # naming data to project name
+    data[ID] = 'SustainLD'
+
+    # get number of cities to prep loop
+    num_of_cities = len(data['@graph'])
+
+    # write ids for every city
+    for city in range(0,num_of_cities):
+        cur_city = data['@graph'][city]
+        cur_city[ID] = cur_city['name']
+
+        cur_city['geo'][ID] = 'geo'
+        cur_city['address'][ID] = 'address'
+
+        num_of_props = len(cur_city['additionalProperty'])
+
+        for prop in range(0,num_of_props):
+            cur_prop = cur_city['additionalProperty'][prop]
+            cur_prop[ID] = cur_prop['name']
+
+# main program
+file_name = 'city-data.jsonld'
+
+data = openFile(file_name)
+update(data)
+storeData(file_name,data)
